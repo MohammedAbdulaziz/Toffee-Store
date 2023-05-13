dc544966d3ff2b3a6a85569b86e1035cd90c8569
MohammedAbdulaziz <mohamed.abaziz980@gmail.com>
2023-05-13 20:51:29 +0300

    Refactored userinput and added comments

--- User/User.java
+++ User/User.java
@@ -1,9 +1,5 @@
 package User;
 
-import java.util.List;
-
-// import Order.Order;
-
 public class User {
     private String username;
     private String password;
@@ -17,10 +13,6 @@ public class User {
         this.username = username;
         this.password = password;
     }
-
-    // public void placeOrder(Order order) {
-    // // implementation
-    // }
 
     public String getUsername() {
         return username;
--- User/UserInput.java
+++ User/UserInput.java
@@ -9,83 +9,115 @@ import java.util.Scanner;
 public class UserInput {
     public static void getUserInput() {
         Scanner myObj = new Scanner(System.in);
+
+        // Ask the user to register or login
         System.out.println("1. Register\n2. Login");
         String choice = myObj.next();
+
         try {
             if (Integer.parseInt(choice) == 1) {
-                System.out.print("Enter new username: ");
-                String username = myObj.next();
-                System.out.print("Enter new password: ");
-                String password = myObj.next();
-                try {
-                    File file = new File("users.txt");
-                    Scanner fileScanner = new Scanner(file);
+                // If the user chose to register, call the register function
+                register(myObj);
 
-                    while (fileScanner.hasNextLine()) {
-                        String line = fileScanner.nextLine();
-                        String[] parts = line.split(":");
-                        String registeredUsername = parts[0];
-
-                        if (username.equals(registeredUsername)) {
-                            System.out.println("Username already exists, please choose another one.");
-                            getUserInput();
-                            return;
-                        }
-                    }
-                    fileScanner.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                User user = new User(username, password);
-                try {
-                    FileWriter writer = new FileWriter("users.txt", true);
-                    writer.write(user.getUsername() + ":" + user.getPassword() + "\n");
-                    writer.close();
-                    System.out.println("Registration successful.");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             } else if (Integer.parseInt(choice) == 2) {
-                System.out.print("Enter username: ");
-                String username = myObj.next();
-                System.out.print("Enter password: ");
-                String password = myObj.next();
-                try {
-                    File file = new File("users.txt");
-                    Scanner fileScanner = new Scanner(file);
-                    boolean userExists = false;
-                    while (fileScanner.hasNextLine()) {
-                        String line = fileScanner.nextLine();
-                        String[] parts = line.split(":");
-                        String registeredUsername = parts[0];
-                        String registeredPassword = parts[1];
+                // If the user chose to login, call the login function
+                login(myObj);
 
-                        if (username.equals(registeredUsername) && password.equals(registeredPassword)) {
-                            System.out.println("Login successful.");
-                            userExists = true;
-                            break;
-                        } else if ((username.equals(registeredUsername) && !password.equals(registeredPassword))) {
-
-                            System.out.println("Incorrect username or password.");
-                            getUserInput();
-                            fileScanner.close();
-                        }
-                    }
-                    if (!userExists) {
-                        System.out.println("User doesn't exist. Please register.");
-                        getUserInput();
-                    }
-                    fileScanner.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
             } else {
+                // If the user entered an invalid choice, prompt again
                 System.out.println("Invalid choice");
                 getUserInput();
             }
+
         } catch (NumberFormatException e) {
+            // If the user entered an invalid choice, prompt again
             System.out.println("Invalid choice");
             getUserInput();
+        }
+    }
+
+    private static void register(Scanner myObj) {
+        // Ask for a new username and password
+        System.out.print("Enter new username: ");
+        String username = myObj.next();
+        System.out.print("Enter new password: ");
+        String password = myObj.next();
+
+        try {
+            File file = new File("users.txt");
+            Scanner fileScanner = new Scanner(file);
+
+            // Check if the username is already taken
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(":");
+                String registeredUsername = parts[0];
+
+                if (username.equals(registeredUsername)) {
+                    System.out.println("Username already exists, please choose another one.");
+                    register(myObj);
+                    return;
+                }
+            }
+            fileScanner.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // If the username is available, create a new User object and write it to the
+        // file
+        User user = new User(username, password);
+        try {
+            FileWriter writer = new FileWriter("users.txt", true);
+            writer.write(user.getUsername() + ":" + user.getPassword() + "\n");
+            writer.close();
+            System.out.println("Registration successful.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void login(Scanner myObj) {
+        // Ask for a username and password
+        System.out.print("Enter username: ");
+        String username = myObj.next();
+        System.out.print("Enter password: ");
+        String password = myObj.next();
+
+        try {
+            File file = new File("users.txt");
+            Scanner fileScanner = new Scanner(file);
+            boolean userExists = false;
+
+            // Check if the username and password match a registered user
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(":");
+                String registeredUsername = parts[0];
+                String registeredPassword = parts[1];
+
+                if (username.equals(registeredUsername) && password.equals(registeredPassword)) {
+                    System.out.println("Login successful.");
+                    userExists = true;
+                    break;
+                } else if ((username.equals(registeredUsername) && !password.equals(registeredPassword))) {
+                    System.out.println("Incorrect username or password.");
+                    login(myObj);
+                    fileScanner.close();
+                }
+            }
+
+            // If the username and password don't match a registered user, either prompt to
+            // register or try again
+            if (!userExists) {
+                System.out.println("User doesn't exist. Please register.");
+                getUserInput();
+            }
+            fileScanner.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
