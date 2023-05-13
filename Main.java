bf46aa5defbec956ee9b92b1a93160435e450734
MohammedAbdulaziz <mohamed.abaziz980@gmail.com>
2023-05-13 20:52:10 +0300

    added comments to main

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ import User.UserInput;
 public class Main {
 
     public static void main(String[] args) {
+        // Get user information
         UserInput.getUserInput();
+
+        // Get order information
         Order.getOrderInput();
     }
 
